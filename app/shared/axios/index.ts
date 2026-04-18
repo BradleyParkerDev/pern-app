d9d12c2cab35a0619f2ad5c471e4dcef270767af
BradleyParkerDev <bradleyscottparker@outlook.com>
2026-04-18 12:18:54 -0500

    fixed api

--- app/shared/axios/index.ts
+++ app/shared/axios/index.ts
@@ -1,14 +1,12 @@
 // app/shared/axios/index.ts
 import axios from 'axios';
 
-// Detect if running in Vite (browser/client)
 const isClient = typeof window !== 'undefined';
 
 const api = axios.create({
 	baseURL: isClient
-		? import.meta.env.VITE_API_URL
+		? '/api'
 		: process.env.API_URL || 'http://localhost:3001/api',
-
 	withCredentials: true,
 });
 
